713efb9479cc55a989968e6c50d29004385cc8d1
Connor Jones <ConnorLeeJones@gmail.com>
2019-06-02 14:23:01 -0400

    Finished

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,114 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+    private String hairColor;
+    private String ssn;
+    private String address;
+    private String occupation;
+    private String phoneNumber;
+
 
     public Person() {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
+        this.hairColor = "";
+        this.ssn = "";
+        this.address = "";
+        this.occupation = "";
+        this.phoneNumber = "";
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
+    public Person(String name, int age, String hairColor, String ssn, String address, String occupation, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.hairColor = hairColor;
+        this.ssn = ssn;
+        this.address = address;
+        this.occupation = occupation;
+        this.phoneNumber = phoneNumber;
+    }
+
+
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+
+
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
+
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -68,6 +68,48 @@ public class TestPerson {
         Assert.assertEquals(expectedName, actualName);
     }
 
+
+    @Test
+    public void testConstructorWithAll() {
+        // Given
+        Integer expectedAge = 5;
+        String expectedName = "Leon";
+
+        String expectedHairColor = "black";
+        String expectedSsn = "111-22-3333";
+        String expectedAddress = "123 Fabricated Cir., PA";
+        String expectedOccupation = "Farmhand";
+        String expectedPhoneNumber = "333-444-5555";
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedHairColor, expectedSsn, expectedAddress, expectedOccupation, expectedPhoneNumber);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+
+        String actualHairColor = person.getHairColor();
+        String actualSsn = person.getSsn();
+        String actualAddress = person.getAddress();
+        String actualOccupation = person.getOccupation();
+        String actualPhoneNumber = person.getPhoneNumber();
+
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+
+        Assert.assertEquals(expectedHairColor, actualHairColor);
+        Assert.assertEquals(expectedSsn, actualSsn);
+        Assert.assertEquals(expectedAddress, actualAddress);
+        Assert.assertEquals(expectedOccupation, actualOccupation);
+        Assert.assertEquals(expectedPhoneNumber, actualPhoneNumber);
+    }
+
+
+
+
+
+
     @Test
     public void testSetName() {
         // Given
@@ -93,6 +135,77 @@ public class TestPerson {
 
         // Then
         Integer actual = person.getAge();
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void setHairColor() {
+        // Given
+        Person person = new Person();
+        String expected = "blonde";
+
+        // When
+        person.setHairColor(expected);
+
+        // Then
+        String actual = person.getHairColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setSsn() {
+        // Given
+        Person person = new Person();
+        String expected = "111-22-3333";
+
+        // When
+        person.setSsn(expected);
+
+        // Then
+        String actual = person.getSsn();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setAddress() {
+        // Given
+        Person person = new Person();
+        String expected = "123 madeUp Dr., Delaware";
+
+        // When
+        person.setAddress(expected);
+
+        // Then
+        String actual = person.getAddress();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setOccupation() {
+        // Given
+        Person person = new Person();
+        String expected = "Software Developer";
+
+        // When
+        person.setOccupation(expected);
+
+        // Then
+        String actual = person.getOccupation();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setPhoneNumber() {
+        // Given
+        Person person = new Person();
+        String expected = "302-111-2222";
+
+        // When
+        person.setPhoneNumber(expected);
+
+        // Then
+        String actual = person.getPhoneNumber();
         Assert.assertEquals(expected, actual);
     }
 }
